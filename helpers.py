7286971a866c6fe2af01c79615390d91bf42c031
JuliaSzulc <me@juliaszulc.com>
2021-08-03 15:37:14 +0200

    swatching implemented

---
+++ helpers.py
@@ -0,0 +1,24 @@
+import seaborn as sns
+import numpy as np
+from matplotlib import ticker
+
+def swatch(base_col, similar_cols):
+    '''Displays evaluated list of similar colors next to the base color.
+    
+    Args:
+    base_col = color code recognized by Seaborn corresponding to the base
+    similar_cols = list of color codes recognized by Seaborn
+    '''
+    sns.set_theme()
+    n = len(similar_cols)
+
+    ax = sns.heatmap([[n] * n, np.arange(n)],
+                     cmap=similar_cols + [base_col],
+                     cbar=False,
+                     square=True)
+
+    for i in range(n):
+        ax.axvline(i, color='white', lw=3)
+        
+    ax.xaxis.set_major_locator(ticker.NullLocator())
+    ax.yaxis.set_major_locator(ticker.NullLocator())
