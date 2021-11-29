87e336486abcf538d99785dbc37adaf90d1d42e8
Julia Szulc <me@juliaszulc.com>
2021-11-29 20:43:21 +0100

    Break lints on purpose

--- src/converters/hexadec.py
+++ src/converters/hexadec.py
@@ -12,11 +12,6 @@ def hex_to_dec_primaries(
     """
     Splits hex color code into three decimal values of the primary colors.
 
-    Args:
-        color (str): Color code string (hex), can include '#' prefix
-        arithmetic (bool, optional): If True returns float values between 0 and 1.
-            Defaults to False.
-
     Returns:
         Union[list[int], list[float]]: List of three integers (0-255) or floats (0-1)
     """
